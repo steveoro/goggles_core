9ec2b837bf8bf94a865226a941f02405acb99c42
steveoro <steve.alloro@gmail.com>
2016-06-18 02:05:26 +0200

    removed admin-only concern class

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 
   module Version
     # Framework Core internal name.
