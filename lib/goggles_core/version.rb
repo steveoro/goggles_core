a2fe5cbfd83faeec7a989c2c1e8e6fd8f4813758
Marco Ligabue <leega@leegax.homenet.telecomitalia.it>
2016-10-25 16:59:14 +0200

    Let's try again

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.0.11"
+  VERSION = "1.0.12"
 
   module Version
     # Framework Core internal name.
