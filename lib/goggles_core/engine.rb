eda9dac281ad3804b0468eb7834b6fe19013231f
steveoro <steve.alloro@gmail.com>
2016-10-25 13:32:09 +0200

    preferring eager_load_paths over autoload_paths

--- lib/goggles_core/engine.rb
+++ lib/goggles_core/engine.rb
@@ -10,18 +10,24 @@ require 'extensions/roman_numeral'
 
 module GogglesCore
   class Engine < ::Rails::Engine
-    # Add load paths for this specific Engine
+    # Add load paths for this specific Engine (prefer eager_load_paths over autoload_paths, since
+    # eager_load_paths are used in production too)
+    config.eager_load_paths << File.expand_path("../../framework", __FILE__)
+    config.eager_load_paths << File.expand_path("../../common", __FILE__)
+    config.eager_load_paths << File.expand_path("../../wrappers", __FILE__)
+    config.eager_load_paths << File.expand_path("../../extensions", __FILE__)
+    config.eager_load_paths << File.expand_path("../../../app/dao", __FILE__)
+    config.eager_load_paths << File.expand_path("../../../app/dao/enhance_individual_ranking_dao", __FILE__)
+    config.eager_load_paths << File.expand_path("../../../app/strategies", __FILE__)
+    config.eager_load_paths << File.expand_path("../../../app/concerns", __FILE__)
     # [Steve A.] When in doubt, open the console and do a...
+    #   $> puts ActiveSupport::Dependencies.eager_load_paths
+    #
+    # ...Or...
+    #
     #   $> puts ActiveSupport::Dependencies.autoload_paths
-    # ...To check the actual resulting paths.
-    config.autoload_paths << File.expand_path("../../framework", __FILE__)
-    config.autoload_paths << File.expand_path("../../common", __FILE__)
-    config.autoload_paths << File.expand_path("../../wrappers", __FILE__)
-    config.autoload_paths << File.expand_path("../../extensions", __FILE__)
-    config.autoload_paths << File.expand_path("../../../app/dao", __FILE__)
-    config.autoload_paths << File.expand_path("../../../app/dao/enhance_individual_ranking_dao", __FILE__)
-    config.autoload_paths << File.expand_path("../../../app/strategies", __FILE__)
-    config.autoload_paths << File.expand_path("../../../app/concerns", __FILE__)
+    #
+    # ...To check for the actual resulting paths.
 
     config.generators do |g|
       g.test_framework :rspec, fixture: false
