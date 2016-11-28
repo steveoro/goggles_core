20fe2f92669fc71203506542c3e997867e990f67
steveoro <steve.alloro@gmail.com>
2016-11-29 00:24:27 +0100

    synch w/ master branch; fix for missing explicit ID field

--- app/strategies/balanced_meeting_score_calculator.rb
+++ app/strategies/balanced_meeting_score_calculator.rb
@@ -8,13 +8,13 @@ require 'wrappers/timing'
 # The "balanced" method is the sum of:
 # - all individual meeting points
 # - best team relays (for category and gender)
-# - bonus points for "missing" athlets 
+# - bonus points for "missing" athlets
 # - bonus points for "missing" relays
 # The bonus points are calculated:
-# - for athlets on the numeber of team athelts 
+# - for athlets on the numeber of team athelts
 #   to the number of athlets of the most numerous team
 # - for relays on the number of team relays considederd
-#   to the maximum number of relays considered for a team   
+#   to the maximum number of relays considered for a team
 #
 # @author   Leega
 # @version  4.00.787
@@ -38,7 +38,7 @@ class BalancedMeetingScoreCalculator
   #++
 
   # Get teams involved in season ranking
-  # 
+  #
   def get_teams
     @teams ||= retrieve_teams
   end
@@ -47,7 +47,7 @@ class BalancedMeetingScoreCalculator
   #++
 
   # Get computed scores
-  # 
+  #
   def get_meeting_team_scores
     @teams = get_teams if not @teams
     @meeting_team_scores ||= compute_team_scores
@@ -65,7 +65,7 @@ class BalancedMeetingScoreCalculator
   def save_computed_score
     persisted_ok = 0
     sql_fields = {}
-    @meeting_team_scores = get_meeting_team_scores if not @meeting_team_scores 
+    @meeting_team_scores = get_meeting_team_scores if not @meeting_team_scores
     @meeting_team_scores.each do |meeting_team_score|
       # Prepare SQL dff statement
       if meeting_team_score.id
@@ -78,10 +78,10 @@ class BalancedMeetingScoreCalculator
         sql_fields['season_individual_points']  = meeting_team_score.sum_individual_points
         sql_fields['season_relay_points']       = meeting_team_score.sum_relay_points
         sql_fields['season_team_points']        = meeting_team_score.sum_team_points
-      
+
         # Save calculated scores
         if meeting_team_score.save
-          persisted_ok += 1 
+          persisted_ok += 1
           sql_diff_text_log << to_sql_update( meeting_team_score, false, sql_fields, "\r\n" )
         end
       else
@@ -152,7 +152,7 @@ class BalancedMeetingScoreCalculator
     meeting_team_scores = []
     max_swimmers_count  = 0
     max_relays_count    = 0
-    
+
     # Computes individuals and relays scores and swimmers and relays total per team
     @teams.each do |team|
       team_score = {}
@@ -166,18 +166,18 @@ class BalancedMeetingScoreCalculator
       team_scores << team_score
 
       # Find out maximum swimmers and relays count
-      max_swimmers_count = team_score[:swimmers_count] if team_score[:swimmers_count] > max_swimmers_count  
-      max_relays_count = team_score[:relays_count] if team_score[:relays_count] > max_relays_count 
+      max_swimmers_count = team_score[:swimmers_count] if team_score[:swimmers_count] > max_swimmers_count
+      max_relays_count = team_score[:relays_count] if team_score[:relays_count] > max_relays_count
     end
-    
+
     # Not necessary in rank not used
     # Computes bonuses
     #team_scores.each do |team_score|
     #  team_score[:swimmers_bonus] = max_swimmers_count - team_score[:swimmers_count]
     #  team_score[:relays_bonus] = max_relays_count - team_score[:relays_count]
     #end
-    
-    # Create meeting team score      
+
+    # Create meeting team score
     team_scores.each_with_index do |team_score, index|
       # Verify if meeting team score already exixts
       meeting_team_score = @meeting.meeting_team_scores.where(['meeting_team_scores.team_id = ?', team_score[:team].id]).first
@@ -187,28 +187,27 @@ class BalancedMeetingScoreCalculator
         meeting_team_score.meeting_id          = @meeting.id
         meeting_team_score.season_id           = @meeting.season_id
         meeting_team_score.team_id             = team_score[:team].id
-        meeting_team_score.team_affiliation_id = team_score[:team].team_affiliations.where(['team_affiliations.season_id = ?', @meeting.season_id])
-        
+        meeting_team_score.team_affiliation_id = team_score[:team].team_affiliations.where(['team_affiliations.season_id = ?', @meeting.season_id]).first.id
         # TODO use current user
         meeting_team_score.user_id             = 2
       end
-      
+
       # Not in use
       meeting_team_score.rank                      = 0
-      
-      # Assigns scores 
+
+      # Assigns scores
       meeting_team_score.sum_individual_points     = team_score[:sum_individual_points]
       meeting_team_score.sum_relay_points          = team_score[:sum_relay_points]
-      meeting_team_score.sum_team_points           = max_swimmers_count - team_score[:swimmers_count] + max_relays_count - team_score[:relays_count] 
+      meeting_team_score.sum_team_points           = max_swimmers_count - team_score[:swimmers_count] + max_relays_count - team_score[:relays_count]
       meeting_team_score.meeting_individual_points = meeting_team_score.sum_individual_points
       meeting_team_score.meeting_relay_points      = meeting_team_score.sum_relay_points
       meeting_team_score.meeting_team_points       = meeting_team_score.sum_team_points
       meeting_team_score.season_individual_points  = meeting_team_score.sum_individual_points
       meeting_team_score.season_relay_points       = meeting_team_score.sum_relay_points
       meeting_team_score.season_team_points        = meeting_team_score.sum_team_points
-      meeting_team_scores << meeting_team_score 
+      meeting_team_scores << meeting_team_score
     end
-    
+
     meeting_team_scores
   end
   #-- --------------------------------------------------------------------------
--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.1.6.0"
+  VERSION = "1.1.7.0"
 
   module Version
     # Framework Core internal name.
@@ -26,10 +26,10 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '019'
+    MINOR   = '020'
 
     # Current build version.
-    BUILD   = '20161128'
+    BUILD   = '20161129'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
