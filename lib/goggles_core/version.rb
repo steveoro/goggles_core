50be72bd00deb420de9a8826df8105f04eaf0d62
steveoro <steve.alloro@gmail.com>
2016-12-13 00:51:37 +0100

    typo fix for meters abbrev.

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.1.10.0"
+  VERSION = "1.1.11.0"
 
   module Version
     # Framework Core internal name.
@@ -26,10 +26,10 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '024'
+    MINOR   = '025'
 
     # Current build version.
-    BUILD   = '20161206'
+    BUILD   = '20161212'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
