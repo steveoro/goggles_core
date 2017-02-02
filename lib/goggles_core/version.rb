aeeef1f856752fd801c55122d262c2f8da137b27
steveoro <steve.alloro@gmail.com>
2017-02-02 14:14:46 +0100

    improved specs for season_creator (no more random failures here)

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.1.25.1"
+  VERSION = "1.1.26.1"
 
   module Version
     # Framework Core internal name.
@@ -26,10 +26,10 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '040'
+    MINOR   = '041'
 
     # Current build version.
-    BUILD   = '20170130'
+    BUILD   = '20170202'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
--- spec/strategies/season_creator_spec.rb
+++ spec/strategies/season_creator_spec.rb
@@ -2,14 +2,26 @@ require 'rails_helper'
 
 
 describe SeasonCreator, type: :strategy do
-  # Needs a season not duplicated
-#  let(:older_season_id) { SeasonType.find_by_code('MASCSI').seasons.sort_season_by_begin_date('DESC').first.id }
-  let(:older_season_id) { SeasonType.find_by_code('MASCSI').seasons.last.id }
-  let(:newer_season_id) { older_season_id + 10 }
-  let(:older_season)    { Season.find( older_season_id ) }
-  let(:description)     { 'Spec proof season ' + newer_season_id.to_s }
 
-  context "with requested parameters" do
+  context "with non-valid parameters," do
+    it "raises an exception for wrong season parameter" do
+      expect{ SeasonCreator.new }.to raise_error( ArgumentError )
+      expect{ SeasonCreator.new( 'only such description' ) }.to raise_error( ArgumentError )
+      expect{ SeasonCreator.new( Season.all.limit(100).sort{0.5 - rand}[0] ) }.to raise_error( ArgumentError )
+      # TODO [Steve, 20160917] This is not true anymore: (free ID helper-finder added)
+#      expect{ SeasonCreator.new( Season.find(131), 'damn, that season was already duplicated' ) }.to raise_error( ArgumentError )
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  context "with valid parameters," do
+    # Needs a UNIQUE season
+    let(:older_season)    { SeasonType.find_by_code('MASCSI').seasons.last }
+    let(:older_season_id) { older_season.id }
+    let(:description)     { "#{FFaker::Lorem.word}-maybe#{ older_season_id + rand * 100}" }
+
     subject { SeasonCreator.new( older_season, description ) }
 
     it_behaves_like( "(the existance of a method)", [
@@ -67,6 +79,7 @@ describe SeasonCreator, type: :strategy do
     #-- -----------------------------------------------------------------------
     #++
 
+
     describe "self.next_header_year," do
       it "returns a valid string" do
         expect( subject.class.next_header_year( subject.older_season.header_year ) ).to be_a_kind_of( String )
@@ -108,183 +121,195 @@ describe SeasonCreator, type: :strategy do
     end
     #-- -----------------------------------------------------------------------
     #++
-
-    describe "#prepare_new_season," do
-      it "returns season, meetings and so on" do
-        expect( subject.new_season ).to be_nil
-        expect( subject.meetings.count ).to eq(0)
-        expect( subject.meeting_sessions.count ).to eq(0)
-        expect( subject.meeting_events.count ).to eq(0)
-        subject.prepare_new_season
-        expect( subject.new_season ).to be_an_instance_of( Season )
-        expect( subject.categories ).to all(be_an_instance_of( CategoryType ))
-        expect( subject.meetings ).to all(be_an_instance_of( Meeting ))
-        expect( subject.meeting_sessions ).to all(be_an_instance_of( MeetingSession ))
-        expect( subject.meeting_events ).to all(be_an_instance_of( MeetingEvent ))
-      end
-    end
-    #-- -----------------------------------------------------------------------
-    #++
-
-    describe "#renew_season," do
-      it "returns a valid season" do
-        expect( subject.renew_season ).to be_an_instance_of( Season )
-      end
-# FIXME THIS FAILS RANDOMLY: (probably due to another spec that creates other fake seasons)
-      it "persists the new season" do
-        subject.renew_season
-        expect( Season.exists?( newer_season_id ) ).to be true
-      end
-    end
-    describe "#new_season," do
-      it "is a valid season" do
-        new_season = subject.renew_season
-        expect( new_season ).to be_an_instance_of( Season )
-      end
-      it "has the calculated id" do
-        new_season = subject.renew_season
-        expect( new_season.id ).to eq( subject.new_id )
-      end
-      it "has the given description" do
-        new_season = subject.renew_season
-        expect( new_season.description ).to eq( subject.description )
-      end
-      it "has the calculated begin date" do
-        new_season = subject.renew_season
-        expect( new_season.begin_date ).to eq( subject.begin_date )
-      end
-      it "has the calculated end date" do
-        new_season = subject.renew_season
-        expect( new_season.end_date ).to eq( subject.end_date )
-      end
-    end
-    #-- -----------------------------------------------------------------------
-    #++
-
-    describe "#renew_categories," do
-      it "returns a collection of category types" do
-        subject.renew_season
-        new_categories = subject.renew_categories
-        expect( new_categories ).to be_a_kind_of( Array )
-        expect( new_categories ).to all(be_an_instance_of( CategoryType ))
-      end
-# FIXME THIS FAILS RANDOMLY: (probably due to another spec that creates other fake seasons)
-      it "returns a collection of category types associated to the new season" do
-        subject.renew_season
-        new_categories = subject.renew_categories
-        new_categories.each do |category_type|
-          expect( category_type.season_id ).to eq( newer_season_id )
-        end
-      end
-      it "returns the same number and types of categories of the older season" do
-        subject.renew_season
-        expect( subject.renew_categories.count ).to eq( older_season.category_types.count )
-      end
-      it "returns the same category types code of older season" do
-        subject.renew_season
-        new_categories = subject.renew_categories
-        new_categories.each do |category_type|
-          expect( older_season.category_types.find_by_code( category_type.code )).to be_an_instance_of( CategoryType )
-        end
-      end
-# FIXME THIS FAILS RANDOMLY: (probably due to another spec that creates other fake seasons)
-      it "persists the new categories" do
-        subject.renew_season
-        subject.renew_categories
-        expect( Season.find(newer_season_id).category_types.count ).to eq( older_season.category_types.count )
-      end
-    end
-    describe "#categories," do
-      it "is a collection of categories" do
-        subject.renew_season
-        subject.renew_categories
-        expect( subject.categories ).to be_a_kind_of( Array )
-        expect( subject.categories ).to all(be_an_instance_of( CategoryType ))
-      end
-    end
-    #-- -----------------------------------------------------------------------
-    #++
-
-    describe "#renew_meetings," do
-      it "returns a collection of meetings" do
-        subject.renew_season
-        new_meetings = subject.renew_meetings
-        expect( new_meetings ).to be_a_kind_of( Array )
-        expect( new_meetings ).to all(be_an_instance_of( Meeting ))
-      end
-      it "returns a collection of meetings associated to the new season" do
-        subject.renew_season
-        new_meetings = subject.renew_meetings
-        new_meetings.each do |meeting|
-          expect( meeting.season_id ).to eq( newer_season_id )
-        end
-      end
-      it "returns the same number of meetings of the older season" do
-        subject.renew_season
-        expect( subject.renew_meetings.count ).to eq( older_season.meetings.count )
-      end
-      it "returns meetings with the given default values" do
-        subject.renew_season
-        new_meetings = subject.renew_meetings
-        new_meetings.each do |meeting|
-          expect( meeting.are_results_acquired ).to eq( false )
-          expect( meeting.is_autofilled ).to eq( true )
-          expect( meeting.has_start_list ).to eq( false )
-          expect( meeting.has_invitation ).to eq( false )
-          expect( meeting.invitation ).to eq( nil )
-          expect( meeting.is_confirmed ).to eq( false )
-        end
-      end
-# FIXME THIS FAILS RANDOMLY: (probably due to another spec that creates other fake seasons)
-      it "persists the new meetings" do
-        subject.renew_season
-        subject.renew_meetings
-        expect( Season.find(newer_season_id).meetings.count ).to eq( older_season.meetings.count )
-      end
-# FIXME THIS FAILS RANDOMLY: (probably due to another spec that creates other fake seasons)
-      it "persists the new meeting sessions" do
-        subject.renew_season
-        subject.renew_meetings
-        expect( Season.find(newer_season_id).meetings.count ).to eq( older_season.meetings.count )
-      end
-    end
-    describe "#meetings," do
-      it "is a collection of meetings" do
-        subject.renew_season
-        subject.renew_meetings
-        expect( subject.meetings ).to be_a_kind_of( Array )
-        expect( subject.meetings ).to all(be_an_instance_of( Meeting ))
-      end
-    end
-    describe "#meeting_sessions," do
-      it "is a collection of meeting_sessions" do
-        subject.renew_season
-        subject.renew_meetings
-        expect( subject.meeting_sessions ).to be_a_kind_of( Array )
-        expect( subject.meeting_sessions ).to all(be_an_instance_of( MeetingSession ))
-      end
-    end
-    describe "#meeting_events," do
-      it "is a collection of meeting_events" do
-        subject.renew_season
-        subject.renew_meetings
-        expect( subject.meeting_events ).to be_a_kind_of( Array )
-        expect( subject.meeting_events ).to all(be_an_instance_of( MeetingEvent ))
-      end
-    end
-    #-- -----------------------------------------------------------------------
-    #++
   end
   #-- -------------------------------------------------------------------------
   #++
 
-  context "without requested parameters" do
-    it "raises an exception for wrong season parameter" do
-      expect{ SeasonCreator.new }.to raise_error( ArgumentError )
-      expect{ SeasonCreator.new( 'only such description' ) }.to raise_error( ArgumentError )
-      expect{ SeasonCreator.new( older_season ) }.to raise_error( ArgumentError )
-      # TODO [Steve, 20160917] This is not true anymore: (free ID helper-finder added)
-#      expect{ SeasonCreator.new( Season.find(131), 'damn, that season was already duplicated' ) }.to raise_error( ArgumentError )
+
+  describe "#prepare_new_season," do
+    # Needs a UNIQUE season
+    let(:older_season)    { SeasonType.find_by_code('MASCSI').seasons.last }
+    let(:older_season_id) { older_season.id }
+    let(:description)     { "#{FFaker::Lorem.word}-maybe#{ older_season_id + rand * 100}" }
+
+    subject { SeasonCreator.new( older_season, description ) }
+
+    it "returns season, meetings and so on" do
+      expect( subject.new_season ).to be_nil
+      expect( subject.meetings.count ).to eq(0)
+      expect( subject.meeting_sessions.count ).to eq(0)
+      expect( subject.meeting_events.count ).to eq(0)
+      subject.prepare_new_season
+      expect( subject.new_season ).to be_an_instance_of( Season )
+      expect( subject.categories ).to all(be_an_instance_of( CategoryType ))
+      expect( subject.meetings ).to all(be_an_instance_of( Meeting ))
+      expect( subject.meeting_sessions ).to all(be_an_instance_of( MeetingSession ))
+      expect( subject.meeting_events ).to all(be_an_instance_of( MeetingEvent ))
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  describe "#renew_season," do
+    # Needs a UNIQUE season
+    let(:older_season)    { SeasonType.find_by_code('MASCSI').seasons.last }
+    let(:older_season_id) { older_season.id }
+    let(:description)     { "#{FFaker::Lorem.word}-maybe#{ older_season_id + rand * 100}" }
+
+    subject { SeasonCreator.new( older_season, description ).renew_season }
+
+    it "returns a valid season" do
+      expect( subject ).to be_an_instance_of( Season )
+      expect( subject ).to be_valid
+    end
+    it "returns a persisted season" do
+      expect( Season.exists?( subject.id ) ).to be true
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  describe "#new_season," do
+    # Needs a UNIQUE season
+    let(:older_season)    { SeasonType.find_by_code('MASCSI').seasons.last }
+    let(:older_season_id) { older_season.id }
+    let(:description)     { "#{FFaker::Lorem.word}-maybe#{ older_season_id + rand * 100}" }
+
+    subject { SeasonCreator.new( older_season, description ) }
+
+    it "is a valid season" do
+      new_season = subject.renew_season
+      expect( new_season ).to be_an_instance_of( Season )
+    end
+    it "has the calculated id" do
+      new_season = subject.renew_season
+      expect( new_season.id ).to eq( subject.new_id )
+    end
+    it "has the given description" do
+      new_season = subject.renew_season
+      expect( new_season.description ).to eq( subject.description )
+    end
+    it "has the calculated begin date" do
+      new_season = subject.renew_season
+      expect( new_season.begin_date ).to eq( subject.begin_date )
+    end
+    it "has the calculated end date" do
+      new_season = subject.renew_season
+      expect( new_season.end_date ).to eq( subject.end_date )
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  describe "#renew_categories," do
+    # Needs a UNIQUE season
+    let(:older_season)    { SeasonType.find_by_code('MASCSI').seasons.last }
+    let(:older_season_id) { older_season.id }
+    let(:description)     { "#{FFaker::Lorem.word}-maybe#{ older_season_id + rand * 100}" }
+    let(:creator)         { SeasonCreator.new( older_season, description ) }
+
+    subject do
+      creator.prepare_new_season
+      creator.categories
+    end
+
+    it "returns a collection of category types" do
+      expect( subject ).to be_a_kind_of( Array )
+      expect( subject ).to all(be_an_instance_of( CategoryType ))
+    end
+    it "returns a collection of category types associated to the new season" do
+      subject.each do |category_type|
+        expect( category_type.season_id ).to eq( creator.new_season.id )
+      end
+    end
+    it "returns the same number and types of categories of the older season" do
+      expect( subject.count ).to eq( older_season.category_types.count )
+    end
+    it "returns the same category types code of older season" do
+      subject.each do |category_type|
+        expect( older_season.category_types.find_by_code( category_type.code )).to be_an_instance_of( CategoryType )
+      end
+    end
+    it "persists the new categories" do
+      expect( subject.count ).to eq( older_season.category_types.count )
+    end
+
+    describe "#categories," do
+      it "is a collection of categories" do
+        expect( subject ).to be_a_kind_of( Array )
+        expect( subject ).to all(be_an_instance_of( CategoryType ))
+      end
+    end
+  end
+  #-- -------------------------------------------------------------------------
+  #++
+
+
+  describe "#renew_meetings," do
+    # Needs a UNIQUE season
+    let(:older_season)    { SeasonType.find_by_code('MASCSI').seasons.last }
+    let(:older_season_id) { older_season.id }
+    let(:description)     { "#{FFaker::Lorem.word}-maybe#{ older_season_id + rand * 100}" }
+    let(:creator)         { SeasonCreator.new( older_season, description ) }
+
+    subject do
+      creator.prepare_new_season
+      creator.meetings
+    end
+
+    it "returns a collection of meetings" do
+      expect( subject ).to be_a_kind_of( Array )
+      expect( subject ).to all(be_an_instance_of( Meeting ))
+    end
+    it "returns a collection of meetings associated to the new season" do
+      subject.each do |meeting|
+        expect( meeting.season_id ).to eq( creator.new_season.id )
+      end
+    end
+    it "returns the same number of meetings of the older season" do
+      expect( subject.count ).to eq( older_season.meetings.count )
+    end
+    it "returns meetings with the given default values" do
+      subject.each do |meeting|
+        expect( meeting.are_results_acquired ).to eq( false )
+        expect( meeting.is_autofilled ).to eq( true )
+        expect( meeting.has_start_list ).to eq( false )
+        expect( meeting.has_invitation ).to eq( false )
+        expect( meeting.invitation ).to eq( nil )
+        expect( meeting.is_confirmed ).to eq( false )
+      end
+    end
+    it "persists the new meetings" do
+      subject
+      expect( Season.find( creator.new_season.id ).meetings.count ).to eq( older_season.meetings.count )
+    end
+    it "persists the new meeting sessions" do
+      subject
+      expect( Season.find( creator.new_season.id ).meetings.count ).to eq( older_season.meetings.count )
+    end
+
+    describe "#meetings," do
+      it "is a collection of meetings" do
+        expect( subject ).to be_a_kind_of( Array )
+        expect( subject ).to all(be_an_instance_of( Meeting ))
+      end
+    end
+
+    describe "#meeting_sessions," do
+      it "is a collection of meeting_sessions" do
+        expect( creator.meeting_sessions ).to be_a_kind_of( Array )
+        expect( creator.meeting_sessions ).to all(be_an_instance_of( MeetingSession ))
+      end
+    end
+
+    describe "#meeting_events," do
+      it "is a collection of meeting_events" do
+        expect( creator.meeting_events ).to be_a_kind_of( Array )
+        expect( creator.meeting_events ).to all(be_an_instance_of( MeetingEvent ))
+      end
     end
   end
   #-- -------------------------------------------------------------------------
