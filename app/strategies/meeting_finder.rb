c76ecaa9c5f12a87a32c015c02bc84d3bc880442
Marco Ligabue <leega@leegax.lan>
2017-02-02 01:15:03 +0100

    Create separate search methods in MeetingFinder Set default to header and swimming pool

--- app/strategies/meeting_finder.rb
+++ app/strategies/meeting_finder.rb
@@ -33,11 +33,32 @@ class MeetingFinder
   #-- --------------------------------------------------------------------------
   #++
 
+  # Executes the search, returning just an array of row IDs, corresponding to the
+  # Meeting rows satisfying the search term.
+  # Default search is set on header and swimming pool
+  #
+  def search_ids(scan_header = true, scan_swimming_pool = true, scan_teams = false, scan_swimmers = false)
+    ids = []
+
+    # Avoid query build-up if no search text is given:
+    if @query_term
+      ids += search_in_header if scan_header
+      ids += search_in_swimming_pool if scan_swimming_pool
+      ids += search_in_teams if scan_teams
+      ids += search_in_swimmers if scan_swimmers
+      
+      # Return the results:
+      ids.uniq
+    else                                            # No search term:
+      Meeting.select(:id).all.map{ |row| row.id }.flatten.uniq
+    end
+  end
+
 
   # Executes the search, returning just an array of row IDs, corresponding to the
   # Meeting rows satisfying the search term.
   #
-  def search_ids
+  def deep_search_ids
     ids = []
     # Avoid query build-up if no search text is given:
     if @query_term
@@ -93,6 +114,138 @@ class MeetingFinder
     else                                            # No search term:
       Meeting.select(:id).all.map{ |row| row.id }.flatten.uniq
     end
+  end
+  #-- --------------------------------------------------------------------------
+  #++
+
+
+  # Executes the search, using meeting header fields
+  # Fileds considered are:
+  # - description
+  # - notes (TODO use team which organize meeting)
+  # Suppressed:
+  # - reference_name
+  # - header_year
+  #
+  # Returns Meeting rows (just an array of row IDs) satisfying the search term.
+  #
+  def search_in_header
+    ids = []
+
+    # Avoid query build-up if no search text is given:
+    if @query_term
+      search_like_text = "%#{@query_term}%"
+
+      # Search among most-used text columns in Meetings:
+          #"(description LIKE ?) OR (header_year LIKE ?) OR (notes LIKE ?) OR (reference_name LIKE ?)",
+      ids += Meeting.select(:id).where(
+        [
+          "(description LIKE ?) OR (notes LIKE ?)",
+          search_like_text, search_like_text
+        ]
+      ).map{ |row| row.id }.flatten.uniq
+    end
+    
+    # Return the results:
+    ids
+  end
+  #-- --------------------------------------------------------------------------
+  #++
+
+
+  # Executes the search, using meeting swimming pools
+  # Fileds considered are:
+  # - pool name
+  # - city name
+  # - country name
+  #
+  # Returns Meeting rows (just an array of row IDs) satisfying the search term.
+  #
+  def search_in_swimming_pool
+    ids = []
+
+    # Avoid query build-up if no search text is given:
+    if @query_term
+      search_like_text = "%#{@query_term}%"
+
+      # Search among pool name and city 
+      ids += Meeting.select(:id)
+          .joins( swimming_pools: :city )
+          .includes( swimming_pools: :city )
+          .where(
+        [
+          "(swimming_pools.name LIKE ?) OR (cities.name LIKE ?) OR (cities.area LIKE ?)",
+          search_like_text, search_like_text, search_like_text
+        ]
+      ).map{ |row| row.id }.flatten.uniq
+    end
+
+    # Return the results:
+    ids
+  end
+  #-- --------------------------------------------------------------------------
+  #++
+
+
+  # Executes the search, evaluating teams involved in meeting
+  # Fileds considered are:
+  # - team name
+  #
+  # Returns Meeting rows (just an array of row IDs) satisfying the search term.
+  #
+  def search_in_teams
+    ids = []
+
+    # Avoid query build-up if no search text is given:
+    if @query_term
+      query_teams_condition    = ConditionsGeneratorColumnStringRegexped.generate_query_conditions(
+        'teams',
+        'name',
+        @query_term
+      )
+      search_like_text = "%#{@query_term}%"
+
+      # Search among linked Teams:
+      ids += Meeting.select(:id)
+          .joins( :teams )
+          .includes( :teams )
+          .where( query_teams_condition ).map{ |row| row.id }.flatten.uniq
+    end
+    
+    # Return the results:
+    ids
+  end
+  #-- --------------------------------------------------------------------------
+  #++
+
+
+  # Executes the search, evaluating swimmers involved in meeting
+  # Fileds considered are:
+  # - swimmer name
+  #
+  # Returns Meeting rows (just an array of row IDs) satisfying the search term.
+  #
+  def search_in_swimmers
+    ids = []
+
+    # Avoid query build-up if no search text is given:
+    if @query_term
+      query_swimmers_condition = ConditionsGeneratorColumnStringRegexped.generate_query_conditions(
+        'swimmers',
+        'complete_name',
+        @query_term
+      )
+      search_like_text = "%#{@query_term}%"
+
+      # Search among linked Swimmers:
+      ids += Meeting.select(:id)
+          .joins( :swimmers )
+          .includes( :swimmers )
+          .where( query_swimmers_condition ).map{ |row| row.id }.flatten.uniq
+    end
+
+    # Return the results:
+    ids
   end
   #-- --------------------------------------------------------------------------
   #++
--- spec/strategies/meeting_finder_spec.rb
+++ spec/strategies/meeting_finder_spec.rb
@@ -7,7 +7,7 @@ require 'meeting_finder'
 describe MeetingFinder, type: :strategy do
 
   it_behaves_like( "(the existance of a method)", [
-    :search_ids, :search
+    :search_ids, :deep_search_ids, :search, :search_in_header, :search_in_swimming_pool, :search_in_teams, :search_in_swimmers
   ] )
   #-- -------------------------------------------------------------------------
   #++
@@ -18,6 +18,11 @@ describe MeetingFinder, type: :strategy do
     describe "#search_ids" do
       it "returns all Meeting rows" do
         expect( subject.search_ids.size ).to eq( Meeting.count )
+      end
+    end
+    describe "#deep_search_ids" do
+      it "returns all Meeting rows" do
+        expect( subject.deep_search_ids.size ).to eq( Meeting.count )
       end
     end
     describe "#search" do
@@ -36,7 +41,11 @@ describe MeetingFinder, type: :strategy do
         expect( subject.search_ids.size ).to eq( Meeting.count )
       end
     end
-
+    describe "#deep_search_ids" do
+      it "returns all Meeting rows" do
+        expect( subject.deep_search_ids.size ).to eq( Meeting.count )
+      end
+    end
     describe "#search" do
       it "returns all Meeting rows" do
         expect( subject.search.count ).to eq( Meeting.count )
@@ -56,7 +65,14 @@ describe MeetingFinder, type: :strategy do
         expect( result_count ).to be < Meeting.count
       end
     end
-
+    describe "#deep_search_ids" do
+      it "returns some Meeting rows not less than search_ids" do
+        result_count = subject.deep_search_ids.size
+        expect( result_count ).to be > 0
+        expect( result_count ).to be < Meeting.count
+        expect( result_count ).to be >= subject.search_ids.size
+      end
+    end
     describe "#search" do
       it "returns more than 1 result with the existing seeds" do
         result_count = subject.search.count
@@ -93,4 +109,89 @@ describe MeetingFinder, type: :strategy do
   end
   #-- -------------------------------------------------------------------------
   #++
+  
+  context "search methods," do
+    subject { MeetingFinder.new("riccione") }
+
+    describe "#search_in_header" do
+      it "returns more than 1 result with the existing seeds" do
+        result_count = subject.search_in_header.count
+        expect( result_count ).to be > 0
+        expect( result_count ).to be < Meeting.count
+      end
+
+      it "returns an empty list if event given" do
+        no_header = MeetingFinder.new("200MI")
+        result = no_header.search_in_header
+        expect( result ).to respond_to(:each)
+        expect( result ).to respond_to(:size)
+        expect( result.size ).to eq( 0 )
+      end
+
+      it "returns an empty list if swimmer given" do
+        no_header = MeetingFinder.new("MARCO LIGABUE")
+        result = no_header.search_in_header
+        expect( result ).to respond_to(:each)
+        expect( result ).to respond_to(:size)
+        expect( result.size ).to eq( 0 )
+      end
+
+      it "returns an empty list if team given" do
+        no_header = MeetingFinder.new("ONDA DELLA PIETRA")
+        result = no_header.search_in_header
+        expect( result ).to respond_to(:each)
+        expect( result ).to respond_to(:size)
+        expect( result.size ).to eq( 0 )
+      end
+    end
+
+    describe "#search_in_swimming_pool" do
+      it "returns more than 1 result with the existing seeds" do
+        result_count = subject.search_in_swimming_pool.count
+        expect( result_count ).to be > 0
+        expect( result_count ).to be < Meeting.count
+      end
+
+      it "returns an empty list if event given" do
+        no_pool = MeetingFinder.new("200MI")
+        result = no_pool.search_in_swimming_pool
+        expect( result ).to respond_to(:each)
+        expect( result ).to respond_to(:size)
+        expect( result.size ).to eq( 0 )
+      end
+
+      it "returns an empty list if swimmer given" do
+        no_pool = MeetingFinder.new("MARCO LIGABUE")
+        result = no_pool.search_in_swimming_pool
+        expect( result ).to respond_to(:each)
+        expect( result ).to respond_to(:size)
+        expect( result.size ).to eq( 0 )
+      end
+
+      it "returns an empty list if header given" do
+        no_pool = MeetingFinder.new("MEETING")
+        result = no_pool.search_in_swimming_pool
+        expect( result ).to respond_to(:each)
+        expect( result ).to respond_to(:size)
+        expect( result.size ).to eq( 0 )
+      end
+    end
+
+    describe "#search_in_teams" do
+      it "returns more than 1 result with the existing seeds" do
+        result_count = subject.search_in_teams.count
+        expect( result_count ).to be > 0
+        expect( result_count ).to be < Meeting.count
+      end
+    end
+
+    describe "#search_in_swimmers" do
+      it "returns more than 1 result with the existing seeds" do
+        ok_swimmer = MeetingFinder.new("LIGABUE")
+        result_count = subject.search_in_swimmers.count
+        expect( result_count ).to be > 0
+        expect( result_count ).to be < Meeting.count
+      end
+    end
+  end
 end
