0394d17be7f21a517d37d9562e06f1fa8cd566ca
Marco Ligabue <leega@leegax.lan>
2017-09-29 08:54:46 +0200

    Fix month deadline for new seasons (9 -> 10)

--- app/models/season.rb
+++ app/models/season.rb
@@ -154,7 +154,7 @@ class Season < ApplicationRecord
   #
   def self.build_header_year_from_date( evaluation_date = Date.today )
     raise ArgumentError.new( "evaluation_date must be of a Date kind." ) unless evaluation_date.kind_of?( Date )
-    year = evaluation_date.month < 9 ? evaluation_date.year - 1 : evaluation_date.year
+    year = evaluation_date.month < 10 ? evaluation_date.year - 1 : evaluation_date.year
     "#{year}/#{year+1}"
   end
 
--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.1.70.2"
+  VERSION = "1.1.70.3"
 
   module Version
     # Framework Core internal name.
@@ -26,10 +26,10 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '136'
+    MINOR   = '137'
 
     # Current build version.
-    BUILD   = '20170928'
+    BUILD   = '20170929'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
