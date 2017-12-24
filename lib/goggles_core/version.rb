1a27a3226888a06394837db2c211c0bae7ffd9d2
Marco Ligabue <leega@leegax.lan>
2017-12-24 19:31:09 +0100

    core version update

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.2.0"
+  VERSION = "1.2.1"
 
   module Version
     # Framework Core internal name.
@@ -26,10 +26,10 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '160'
+    MINOR   = '161'
 
     # Current build version.
-    BUILD   = '20171129'
+    BUILD   = '20171224'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
