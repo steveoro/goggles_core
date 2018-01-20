8ef7048380ee93a9ddc62a96c2856cbe2ade5289
Marco Ligabue <leega@leegax.lan>
2018-01-20 18:53:38 +0100

    Reverto sort optimization try

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.3.5"
+  VERSION = "1.3.6"
 
   module Version
     # Framework Core internal name.
