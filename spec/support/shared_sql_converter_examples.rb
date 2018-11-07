1aadef07afdfa82af1b5d6d8df4aef7507d69ccb
Marco Ligabue <leega@leegax.lan>
2018-11-07 11:25:11 +0100

    shared spec update

--- spec/support/shared_sql_converter_examples.rb
+++ spec/support/shared_sql_converter_examples.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 
 shared_examples_for "SqlConverter [param: let(:record)]" do
 
+  let(:to_ignore) { ['lock_version', 'tags_by_user_list', 'tags_by_team_list'] }
+
   it_behaves_like( "(the existance of a method)", [
     :to_sql_insert, :to_sql_update, :to_sql_delete,
     :destroy_with_sql_capture
@@ -17,7 +19,8 @@ shared_examples_for "SqlConverter [param: let(:record)]" do
       expect( sql_text ).to include( record.class.table_name )
     end
     it "contains the list of values of the record (expcept :lock_version)" do
-      required_attributes = record.attributes.reject{ |key| key == 'lock_version' }
+      #required_attributes = record.attributes.reject{ |key| key == 'lock_version' }
+      required_attributes = record.attributes.reject{ |key| to_ignore.include?(key) }
       quoted_values       = required_attributes.values.map{ |value| record.class.connection.quote(value) }
       sql_text            = subject.to_sql_insert(record)
       expect( sql_text ).to include( quoted_values.join(', ') )
@@ -33,7 +36,8 @@ shared_examples_for "SqlConverter [param: let(:record)]" do
       expect( sql_text ).to include( record.class.table_name )
     end
     it "contains the list of values of the record (expcept :id & :lock_version)" do
-      required_attributes = record.attributes.reject{ |key| key == 'id' || key == 'lock_version' }
+      #required_attributes = record.attributes.reject{ |key| key == 'id' || key == 'lock_version' }
+      required_attributes = record.attributes.reject{ |key| key == 'id' || to_ignore.include?(key) }
       quoted_values       = required_attributes.values.map{ |value| record.class.connection.quote(value) }
       sql_text            = subject.to_sql_update(record)
       quoted_values.each do |value|
