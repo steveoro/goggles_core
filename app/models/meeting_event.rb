b85e10eed18160c4cc4359817e059649730942a1
Marco Ligabue <leega@leegax.lan>
2019-03-09 17:22:39 +0100

    Add has_many reservations to meeting_event

--- app/models/meeting_event.rb
+++ app/models/meeting_event.rb
@@ -36,6 +36,9 @@ class MeetingEvent < ApplicationRecord
   has_many :meeting_individual_results, through: :meeting_programs
   has_many :meeting_relay_results,      through: :meeting_programs
 
+  has_many :meeting_event_reservations, dependent: :delete_all
+  has_many :meeting_relay_reservations, dependent: :delete_all
+
   has_many :category_types, through: :meeting_programs
 
 
