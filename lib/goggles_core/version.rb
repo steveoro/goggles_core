03f53fd36ce5fcf823f2a336bbf3263b034c51fe
Leega <leegaweb@gmail.com>
2019-05-26 14:41:05 +0200

    Fix eir

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = "1.3.95"
+  VERSION = "1.3.96"
 
   module Version
     # Framework Core internal name.
