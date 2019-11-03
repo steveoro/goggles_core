ae0a48b1dbf707344a306976363aa3389dd1822e
steveoro <steve.alloro@gmail.com>
2019-11-03 23:24:00 +0100

    fuubar for Rubocop under Guard; v. 423

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = '1.4.22'
+  VERSION = '1.4.23'
 
   module Version
     # Framework Core internal name.
@@ -26,7 +26,7 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '422'
+    MINOR   = '423'
 
     # Current build version.
     BUILD   = '20191103'
