b971ba1a943fba9a00abfe4dc565f0d4a870779a
steveoro <steve.alloro@gmail.com>
2019-11-07 22:57:09 +0100

    fix parameter in mysqldump call

--- lib/goggles_core/version.rb
+++ lib/goggles_core/version.rb
@@ -14,7 +14,7 @@ module GogglesCore
   #++
 
   # Actual Core versioning, used for Gem differentiation.
-  VERSION = '1.4.23'
+  VERSION = '1.4.25'
 
   module Version
     # Framework Core internal name.
@@ -26,10 +26,10 @@ module GogglesCore
     MAJOR   = '6'
 
     # Minor version.
-    MINOR   = '423'
+    MINOR   = '425'
 
     # Current build version.
-    BUILD   = '20191103'
+    BUILD   = '20191107'
 
     # Full versioning for the current release (Framework + Core).
     FULL    = "#{MAJOR}.#{MINOR}.#{BUILD} (#{CORE} v. #{VERSION})"
